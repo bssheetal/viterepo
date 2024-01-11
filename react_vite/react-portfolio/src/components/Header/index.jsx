54ca33659ef0f5bd2a7c9360a25a6826ee6525e0
Sheetal <sheetal.srikumar@gmail.com>
2024-01-10 19:03:23 -0800

    saving changes

---
+++ react_vite/react-portfolio/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+
+
+function Header(props) {
+
+  return (
+    <header className="flex-row space-between px-1">
+      <h1>Lernantino</h1>
+      
+    </header>
+  );
+}
+
+export default Header;
